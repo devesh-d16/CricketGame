bc217b7dc21c012b33edc5cb625b893f339d0ab0
Devesh Dewangan <dewangandevesh1602.com>
2025-01-21 14:41:37 +0530

    made team and cricket class better, handled some exceptions and made a UI class

--- src/Team.java
+++ src/Team.java
@@ -1,11 +1,33 @@
 public class Team {
-    int score;
-    int wicket;
-    String name;
+    private int score;
+    private int wicket;
+    private final String name;
 
     public Team(String name){
         score = 0;
         wicket = 0;
         this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addScore(int runs) {
+        this.score += runs;
+    }
+
+    public int getWicket() {
+        return wicket;
+    }
+
+    public void addWicket() {
+        if(this.wicket < 10){
+            this.wicket++;
+        }
+    }
+
+    public String getName() {
+        return name;
     }
 }
